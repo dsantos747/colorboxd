fe231d466284fc37648ffb4ac2c5d1a4aea6d01c
dsantos747 <d.santos74747@gmail.com>
2024-02-06 15:26:48 +0000

    Converted frontend to TS; created coming soon page

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src='logo512_clear.png' className='App-logo' alt='logo' />
+        <p>ColorBoxd is under construction.</p>
+        <br></br>
+        <p>Please check back soon!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
