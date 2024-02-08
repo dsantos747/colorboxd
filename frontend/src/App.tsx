7e17a1692ee4fa9ae93d7d50ddba5f8b2ebd66a5
dsantos747 <d.santos74747@gmail.com>
2024-02-08 09:45:37 +0000

    Added tailwind; initial build for docker

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,8 @@
-import './App.css';
-
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <img src='logo512_clear.png' className='App-logo' alt='logo' />
+    <div className='text-center'>
+      <header className='bg-gray-900 min-h-screen flex flex-col items-center justify-center text-white text-2xl'>
+        <img src='logo512_clear.png' className='h-[40vmin] pointer-events-none' alt='logo' />
         <p>ColorBoxd is under construction.</p>
         <br></br>
         <p>Please check back soon!</p>
