72418a4169b70921a845c6431f79f75a9d27ba1f
dsantos747 <d.santos74747@gmail.com>
2024-03-13 13:44:04 +0000

    Fixed nav auth button bug

--- frontend/src/ui/nav.tsx
+++ frontend/src/ui/nav.tsx
@@ -1,16 +1,9 @@
-import { useContext } from 'react';
-import { UserTokenContext, UserTokenContextType } from '../lib/contexts';
 import { LoginButton, SignOutButton } from './buttons';
 import ColorboxdLogo from './colorboxdLogo';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Nav = () => {
-  // Theres a glitch where the sign out button stays visible when navigating to home,
-  // but then when you refresh it's back to log in. To make things easier, it's probably
-  // easier to just have a "Get started" button (navigate to users page) and a "sign out"
-  // button (clear auth token) - with the sign out button only visible on the user page
-
-  const { userToken } = useContext(UserTokenContext) as UserTokenContextType;
+  const location = useLocation();
 
   return (
     <nav className='fixed w-screen bg-gradient-to-b from-gray-900 from-70% to-transparent z-10'>
@@ -20,16 +13,7 @@ const Nav = () => {
             <ColorboxdLogo />
           </h1>
         </Link>
-        {userToken && (
-          <div className='text-white'>
-            <SignOutButton />
-          </div>
-        )}
-        {!userToken && (
-          <div className='text-white'>
-            <LoginButton />
-          </div>
-        )}
+        {location.pathname.toLowerCase().startsWith('/user') ? <SignOutButton /> : <LoginButton />}
       </div>
     </nav>
   );
