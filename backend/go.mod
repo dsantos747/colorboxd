d6350589212b44b6a463f1e8a952b424bd1ff6d6
dsantos747 <d.santos74747@gmail.com>
2024-04-10 17:48:44 +0100

    Added getAuthCode helper function for tests

--- backend/go.mod
+++ backend/go.mod
@@ -13,6 +13,7 @@ require (
 require (
 	cloud.google.com/go/functions v1.15.1 // indirect
 	github.com/cloudevents/sdk-go/v2 v2.14.0 // indirect
+	github.com/go-rod/rod v0.114.8 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
@@ -20,6 +21,11 @@ require (
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/oliamb/cutter v0.2.2 // indirect
 	github.com/stretchr/testify v1.8.4 // indirect
+	github.com/ysmood/fetchup v0.2.4 // indirect
+	github.com/ysmood/goob v0.4.0 // indirect
+	github.com/ysmood/got v0.39.4 // indirect
+	github.com/ysmood/gson v0.7.3 // indirect
+	github.com/ysmood/leakless v0.8.0 // indirect
 	go.uber.org/atomic v1.4.0 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
 	go.uber.org/zap v1.10.0 // indirect
--- backend/main_test.go
+++ backend/main_test.go
@@ -1,6 +1,20 @@
 package colorboxd
 
-import "testing"
+import (
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/go-rod/rod"
+	"github.com/go-rod/rod/lib/input"
+	"github.com/go-rod/rod/lib/launcher"
+	"github.com/go-rod/rod/lib/proto"
+	"github.com/joho/godotenv"
+)
+
+// Letterboxd member id: 67W7X
+// Letterboxd Test List id: tqtA2
 
 func TestLoadImage(t *testing.T) {
 	_, err := loadImage("https://www.colorhexa.com/ff0000.png")
@@ -27,7 +41,6 @@ func TestGetRedImageInfo(t *testing.T) {
 	redImageInfo, err := getImageInfo(entry, redImage)
 	if err != nil {
 		t.Errorf("Error getting image info: %v\n", err)
-		return
 	}
 
 	if redImageInfo.ImageInfo.Colors[0].hex != expectedHex {
@@ -37,3 +50,78 @@ func TestGetRedImageInfo(t *testing.T) {
 		t.Errorf("Expected hue %v, computed %v.", expectedHue, redImageInfo.ImageInfo.Colors[0].h)
 	}
 }
+
+func TestGetAccessToken(t *testing.T) {
+	authCode, err := getAuthCode()
+	if err != nil {
+		t.Errorf("failed to generate auth code: %v", err)
+	}
+
+	accessTokenResponse, err := getAccessToken(*authCode)
+	if err != nil {
+		t.Errorf("could not create valid access token for provided auth code: %v", err)
+	}
+
+	if accessTokenResponse.AccessToken == "" {
+		t.Errorf("no valid access token present in response: %v", err)
+	}
+
+	member, err := getMemberId(accessTokenResponse.AccessToken)
+	if err != nil {
+		t.Errorf("could not retrieve member ID: %v", err)
+	}
+
+	fmt.Println(member.DisplayName)
+}
+
+// Helper function to log in to letterboxd account and return an auth code.
+// This works because I have already authorised colorboxd for my account.
+func getAuthCode() (*string, error) {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("Could not load environment variables from .env file: %v\n", err)
+		return nil, err
+	}
+
+	launcher := launcher.New()
+	launcher.Leakless(false)
+	u := launcher.MustLaunch()
+
+	browser := rod.New().ControlURL(u).MustConnect()
+	defer browser.MustClose()
+
+	authCodeChan := make(chan string)
+
+	go func() {
+		_ = browser.EachEvent(func(e *proto.NetworkResponseReceived) {
+			if strings.Contains(e.Response.URL, "https://colorboxd.com/user?code=") {
+				// Parse the URL to extract the auth code
+				parts := strings.Split(e.Response.URL, "=")
+				if len(parts) > 1 {
+					authCode := parts[1]
+					fmt.Println("Received auth code:", authCode)
+					authCodeChan <- authCode
+				}
+			}
+		})
+	}()
+
+	// Create a new page
+	page := browser.MustPage(os.Getenv("LBOXD_AUTH_URL")).MustWaitStable()
+
+	// Enter login details
+	page.MustElement("#field-username").MustInput(os.Getenv("MY_LBOXD_USER"))
+	page.MustElement("#field-password").MustInput(os.Getenv("MY_LBOXD_PASS")).MustType(input.Enter)
+
+	page.MustWaitNavigation()
+
+	authCode := <-authCodeChan
+
+	browser.MustClose()
+	launcher.Kill()
+
+	return &authCode, nil
+}
+
+// getUserLists test
+// To do this, first need to generate an access token
