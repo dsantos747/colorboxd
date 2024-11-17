81d2fee6c69ea61a4cca99e094f1cfed83071406
Daniel Santos <d.santos74747@gmail.com>
2024-11-17 17:53:18 +0100

    quick authUser v2

--- backend/v2/go.mod
+++ backend/v2/go.mod
@@ -2,4 +2,7 @@ module github.com/dsantos747/letterboxd_hue_sort/backend/v2
 
 go 1.23.2
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+)
--- backend/v2/utils.go
+++ backend/v2/utils.go
@@ -2,7 +2,10 @@ package colorboxd
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 	"os"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -18,4 +21,39 @@ func LoadEnv() error {
 		}
 	}
 	return nil
+}
+
+// ReturnError sends a http error back to the ResponseWriter w
+func ReturnError(w http.ResponseWriter, message string, statusCode int) {
+	w.Header().Set("Content-Type", "text/plain")
+	http.Error(w, message, statusCode)
+}
+
+var HTTPclient = &http.Client{
+	Timeout: time.Second * 30,
+}
+
+// Makes an HTTP request of the required method to the specified endpoint.
+// If response code is >= 400 , returns an error with response.Status
+func MakeHTTPRequest(method, endpoint string, body io.Reader, headers map[string]string) (*http.Response, error) {
+	// Prepare the request
+	req, err := http.NewRequest(method, endpoint, body)
+	if err != nil {
+		return nil, err
+	}
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+
+	// Perform the request
+	response, err := HTTPclient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	if response.StatusCode >= 400 {
+		err = fmt.Errorf("%s", response.Status)
+		return nil, err
+	}
+
+	return response, nil
 }
